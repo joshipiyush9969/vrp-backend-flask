88e7be9d090a38f390ac709565a85cf0087feb44
Piyush Joshi <piyushjb7@hotmail.com>
2022-09-14 22:40:32 +0530

    intial

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+ 
+@app.route('/')
+def hello_world():
+    return 'Major OMG'
+ 
+# main driver function
+if __name__ == '__main__':
+    app.run(debug=True)
